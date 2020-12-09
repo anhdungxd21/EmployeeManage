9caa2daa2574f59499041af6ed9285c8f83a3dad
= <=>
2020-12-09 10:38:44 +0700

    init something

---
+++ src/employee/Employee.java
@@ -0,0 +1,51 @@
+package employee;
+
+public class Employee {
+    private  final String COMMA = ",";
+    private int id;
+    private String name;
+    private String position;
+    private int dayInWork = 0;
+    public Employee(int id, String name, String position){
+        this.id = id;
+        this.name = name;
+        this.position = position;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getDayInWork() {
+        return dayInWork;
+    }
+
+    public void setDayInWork(int dayInWork) {
+        this.dayInWork = dayInWork;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + COMMA + this.name + COMMA + this.position + COMMA + this.dayInWork;
+    }
+}
---
+++ test/employee/EmployeeTest.java
@@ -0,0 +1,16 @@
+package employee;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @Test
+    void testToString() {
+        Employee employee = new Employee(1,"dung","GD");
+        String[] expect = {"1","dung","GD","0"};
+        String[] result = employee.toString().split(",");
+        assertArrayEquals(expect,result);
+    }
+}
